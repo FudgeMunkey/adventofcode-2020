c0a7b519b578e711418c554f464e503db0672657
Alex Horvat <u6048805@anu.edu.au>
2020-12-08 16:28:24 +1100

    Update stub file

--- src/stub.py
+++ src/stub.py
@@ -1,2 +1,18 @@
+def read_input():
+    # Read input
+    f = open("puzzle_input.txt", "r")
+    raw_data = f.read().split('\n')
+    clean_data = []
+
+    # Clean Data
+    clean_data = raw_data[:-1]
+
+    return clean_data
+
+
 if __name__ == "__main__":
-    pass
+    data = read_input()
+
+    # Solve problem
+    for i in range(len(data)):
+        pass
